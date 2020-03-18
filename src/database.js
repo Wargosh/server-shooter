5d951efd2ce05c901b88fac4d945d2b686d80284
Wargosh <erick.jara@unl.edu.ec>
2020-03-18 00:15:33 -0500

    Conección a BD Azure

--- src/database.js
+++ src/database.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/shooter-db', { // local
+//Local
+// mongoose.connect('mongodb://localhost/shooter-db', {
 
-        // Server in Heroku....
-        // mongoose.connect('mongodb+srv://Wargosh:Wargosh30@cluster0-0vnzr.mongodb.net/test?retryWrites=true&w=majority', {
+// Server in Heroku....
+// mongoose.connect('mongodb+srv://Wargosh:Wargosh30@cluster0-0vnzr.mongodb.net/test?retryWrites=true&w=majority', {
 
-        // Server in azure....
-        //
+// Server in azure....
+mongoose.connect('mongodb://shooter-db:MeUCwsgAkQ4hprG1KKSLQPetBWKQRzCTyB9Buf4tUGrcJcq9yAoEZHLj9PTqKLzP2AIciPg2tm1rpBPkFBJg6g==@shooter-db.mongo.cosmos.azure.com:10255/shooter-db?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@shooter-db@', {
 
         useCreateIndex: true,
         useNewUrlParser: true,
