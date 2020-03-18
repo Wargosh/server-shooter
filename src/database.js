e8fc9f581a04548bbfddf6db90c2a02ceaa4196a
Wargosh <erick.jara@unl.edu.ec>
2020-03-18 16:57:34 -0500

    Fig bug on request to chat global

--- src/database.js
+++ src/database.js
@@ -1,13 +1,14 @@
 const mongoose = require('mongoose');
+// var env = require('dotenv').config();
 
 //Local
 // mongoose.connect('mongodb://localhost/shooter-db', {
 
 // Server in Heroku....
-// mongoose.connect('mongodb+srv://Wargosh:Wargosh30@cluster0-0vnzr.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://Wargosh:Wargosh30@cluster0-0vnzr.mongodb.net/test?retryWrites=true&w=majority', {
 
-// Server in azure....
-mongoose.connect('mongodb://shooter-db:MeUCwsgAkQ4hprG1KKSLQPetBWKQRzCTyB9Buf4tUGrcJcq9yAoEZHLj9PTqKLzP2AIciPg2tm1rpBPkFBJg6g==@shooter-db.mongo.cosmos.azure.com:10255/shooter-db?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@shooter-db@', {
+        // Server in azure....
+        //mongoose.connect('mongodb://shooter-db:MeUCwsgAkQ4hprG1KKSLQPetBWKQRzCTyB9Buf4tUGrcJcq9yAoEZHLj9PTqKLzP2AIciPg2tm1rpBPkFBJg6g==@shooter-db.mongo.cosmos.azure.com:10255/shooter-db?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@shooter-db@', {
 
         useCreateIndex: true,
         useNewUrlParser: true,
@@ -16,3 +17,13 @@ mongoose.connect('mongodb://shooter-db:MeUCwsgAkQ4hprG1KKSLQPetBWKQRzCTyB9Buf4tU
     })
     .then(db => console.log('DB is connected!'))
     .catch(err => console.log(err));
+/*
+// Server in azure....
+    mongoose.connect("mongodb://" + process.env.COSMOSDB_HOST + ":" + process.env.COSMOSDB_PORT + "/" + process.env.COSMOSDB_DBNAME + "?ssl=true&replicaSet=globaldb", {
+        auth: {
+            user: process.env.COSMODDB_USER,
+            password: process.env.COSMOSDB_PASSWORD
+        }
+    })
+    .then(() => console.log('DB is connected!'))
+    .catch((err) => console.error(err));*/
--- src/routes/chats.js
+++ src/routes/chats.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 
 const Chat = require('../models/Chat');
 
-router.post('/chat/global/get_messages', async (req, res) => {
+router.post('/chat/global/get_messages', async(req, res) => {
     const messages = await Chat.find({ type: 'global' }).sort({ created_at: 'desc' }).limit(10);
     if (messages) {
         res.send({ messages: messages });
     } else {
-        res.send({ error: 'Ha ocurrido un error' });
+        res.send({ error: 'Ha ocurrido un error al intentar obtener EL CHAT GLOBAL' });
     }
 });
 
