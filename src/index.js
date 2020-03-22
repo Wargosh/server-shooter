01462145f451e120c03fb88125033f111b68663a
Wargosh <erick.jara@unl.edu.ec>
2020-03-22 05:35:11 -0500

    fix bug in rooms

--- src/index.js
+++ src/index.js
@@ -170,6 +170,27 @@ io.on('connection', (socket) => {
                     }
                     break;
                 }
+            }
+        }
+
+        for (var r in rooms) {
+            const auxRoom = io.sockets.adapter.rooms[r];
+            if (auxRoom) {
+                if (auxRoom.length < 2) { // establesco un limite de usuarios por sala
+                    banRoom = true;
+                    socket.join(r); // unirse a esta sala
+                    // rooms[r] = { id_room: r };
+                    roomGame = r;
+                    players[thisPlayerId].roomGame = roomGame;
+
+                    // mantener al cliente en cola, hasta cumplir la condición
+                    const statusRoom = io.sockets.adapter.rooms[r];
+                    if (statusRoom.length == 2) {
+                        socket.in(roomGame).broadcast.emit('game:start', { message: 'OK' });
+                        socket.emit('game:start', { message: 'OK' });
+                    }
+                    break;
+                }
             } else {
                 banRoom = true;
                 socket.join(r); // unirse a esta sala
@@ -224,6 +245,7 @@ io.on('connection', (socket) => {
             p.status_player = "online";
             await p.save();
         }
+        socket.nickname = data.username;
         players[thisPlayerId].username = data.username;
 
         socket.broadcast.emit('player:online', { id: thisPlayerId, user: data.username });
